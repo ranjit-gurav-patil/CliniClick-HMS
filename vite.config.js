1d996053d780f7714b9b4ff04bd7fece8ef39f2d
unknown <ranjit.gurav.patil@gmail.com>
2026-05-30 18:48:36 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this exact line. It must match your GitHub repository name exactly!
+  base: '/CliniClick-HMS/', 
 })
